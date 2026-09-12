bfee1f5360f6f9860ddd6d6ff447ee74c9406fc0
ravijana71-m <ravijana71@gmail.com>
2026-09-12 19:02:36 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -7,9 +7,9 @@ resource "aws_instance" "AWSINSTANCE"{
 ami = "ami-08188a5a4dfdbd573"
 instance_type = "t3.micro"
 key_name = "devopsravi2026"
-security_groups = ["launch-wizard-1"]
+security_groups = ["launch-wizard-11"]
 tags = {
-Name = "terraformserver"
+Name = "terraformserver-1"
 }
 }
 
